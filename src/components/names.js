4490e1e33546a1d93cd944860ad31cd5de569d91
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-09-19 22:24:54 -0300

    testing linters

--- src/components/names.js
+++ src/components/names.js
@@ -1,9 +1,20 @@
 import PropTypes from 'prop-types';
 
-function Names({ name }) {
-  return <p className="my-name">{name}</p>;
+function Names({ name, age }) {
+  return (
+    <p className="my-name">
+      {name}
+      {' '}
+      and
+      {' '}
+      {age}
+    </p>
+  );
 }
 
-Names.propTypes = { name: PropTypes.string.isRequired };
+Names.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+};
 
 export default Names;
