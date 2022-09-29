bcf9a3b814800fa4e722f9931cd17d777aa4d77c
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-09-29 01:26:13 -0300

    Create navbar and refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import Calculator from './components/calculator';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 const App = () => (
-  <div className="App">
-    <Calculator />
-    <Home />
-  </div>
+  <Router className="App">
+    <Navbar />
+    <Routes>
+      <Route path="Calculator" element={<Calculator />} />
+      <Route path="Home" element={<Home />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
