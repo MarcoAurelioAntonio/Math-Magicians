fe8aa9d7b340c124fd7cfe22c5da4c5c05aef6a3
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-09-29 01:12:14 -0300

    home component and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import Calculator from './components/calculator';
+import Home from './components/Home';
 
 const App = () => (
   <div className="App">
     <Calculator />
+    <Home />
   </div>
 );
 
