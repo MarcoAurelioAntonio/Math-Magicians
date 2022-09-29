94e821af5dc0d8eb5e0082a4777e397810ad8c29
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-09-28 22:08:51 -0300

    Refactored components code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import './App.css';
-import React from 'react';
-import Calculator from './components/UI/Calculator';
+import React, { useState } from 'react';
+import Button from './components/UI/Calculator';
+import calculate from './logic/calculate';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
+function App() {
+  const [state, setState] = useState({
+    total: 0,
+    operation: null,
+    next: null,
+    error: null,
+  });
+
+  const functionClick = (event) => {
+    setState((prevState) => calculate(prevState, event.target.textContent));
+  };
+
+  return (
+    <div className="App">
+      <div className="in">
+        { state.total }
+        { state.operation }
+        { state.next }
+        { state.error }
       </div>
-    );
-  }
+      <div>
+        <Button buttonText="AC" handleClick={functionClick} />
+        <Button buttonText="-/+" handleClick={functionClick} />
+        <Button buttonText="%" handleClick={functionClick} />
+        <Button buttonText="+" handleClick={functionClick} />
+      </div>
+      <div>
+        <Button buttonText="0" handleClick={functionClick} />
+        <Button buttonText="1" handleClick={functionClick} />
+        <Button buttonText="2" handleClick={functionClick} />
+        <Button buttonText="3" handleClick={functionClick} />
+        <Button buttonText="4" handleClick={functionClick} />
+        <Button buttonText="5" handleClick={functionClick} />
+        <Button buttonText="6" handleClick={functionClick} />
+        <Button buttonText="7" handleClick={functionClick} />
+        <Button buttonText="8" handleClick={functionClick} />
+        <Button buttonText="9" handleClick={functionClick} />
+      </div>
+      <div>
+        <Button buttonText="=" handleClick={functionClick} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/UI/Calculator.js
+++ src/components/UI/Calculator.js
@@ -1,56 +1,23 @@
-import React, { useState } from 'react';
-import calculate from '../../logic/calculate';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Calculator() {
-  const [state, setState] = useState({
-    total: 0,
-    operation: null,
-    next: null,
-    error: null,
-  });
-
-  const handleClick = (event) => {
-    setState((prevState) => calculate(prevState, event.target.textContent));
-  };
+function Button({ buttonText, handleClick }) {
+  const isOperator = (valor) => Number.isNaN(valor) && (valor !== '.') && (valor !== '=');
 
   return (
-    <section>
-      <div className="calculator">
-
-        <div className="in">
-          { state.total }
-          { state.operation }
-          { state.next }
-          { state.error }
-        </div>
-
-        {/* operators */}
-        <button onClick={handleClick} className="di y" type="button">÷</button>
-        <button onClick={handleClick} className="mu y x" type="button">x</button>
-        <button onClick={handleClick} className="mi y" type="button">-</button>
-        <button onClick={handleClick} className="pl y" type="button">+</button>
-        <button onClick={handleClick} className="eq y" type="button">=</button>
-
-        {/* methods */}
-        <button onClick={handleClick} className="ac a" type="button">AC</button>
-        <button onClick={handleClick} className="pm a" type="button">+/-</button>
-        <button onClick={handleClick} className="pc a" type="button">%</button>
-
-        {/* numbers */}
-        <button onClick={handleClick} className="n9 a" type="button">9</button>
-        <button onClick={handleClick} className="n8 a" type="button">8</button>
-        <button onClick={handleClick} className="n7 a" type="button">7</button>
-        <button onClick={handleClick} className="n6 a" type="button">6</button>
-        <button onClick={handleClick} className="n5 a" type="button">5</button>
-        <button onClick={handleClick} className="n4 a" type="button">4</button>
-        <button onClick={handleClick} className="n3 a" type="button">3</button>
-        <button onClick={handleClick} className="n2 a" type="button">2</button>
-        <button onClick={handleClick} className="n1 a" type="button">1</button>
-        <button onClick={handleClick} className="n0 a" type="button">0</button>
-        <button onClick={handleClick} className="do a" type="button">.</button>
-      </div>
-    </section>
+    <div
+      className={`button-container ${isOperator(buttonText) ? 'operator' : ''}`.trimEnd()}
+      onClick={handleClick}
+      aria-hidden="true"
+    >
+      {buttonText}
+    </div>
   );
 }
 
-export default Calculator;
+Button.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
+
+export default Button;
